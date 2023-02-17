8d4a95199f8225283ea094d8a46f612b5b9034f3
deep-quality-dev <hungrywarrior081@gmail.com>
2023-02-17 06:24:58 -0500

    Upgraded contract v2,v3

---
+++ scripts/impl.ts
@@ -0,0 +1,37 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { ethers, network } from "hardhat";
+import { DeHubTokenV3__factory } from "../typechain";
+import { verifyContract } from "./helpers";
+
+const main = async () => {
+  const signers = await ethers.getSigners();
+  if (signers.length < 1) {
+    throw new Error(`Not found deployer`);
+  }
+
+  const deployer: SignerWithAddress = signers[0];
+  console.log(`Using deployer address ${deployer.address}`);
+
+  if (
+    network.name === "mainnet" ||
+    network.name === "goerli" ||
+    network.name === "polygon" ||
+    network.name === "polygonMumbai" ||
+    network.name === "bsc" ||
+    network.name === "bscTestnet"
+  ) {
+    const DeHubTokenFactory = new DeHubTokenV3__factory(deployer);
+    const dehubToken = await DeHubTokenFactory.deploy();
+    await dehubToken.deployed();
+
+    console.log(`DeHubToken deployed at ${dehubToken.address}`);
+    await verifyContract(dehubToken.address);
+  }
+};
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
